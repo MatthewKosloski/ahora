5f39631b4698caec5edb88f331fff2245cc84ada
MatthewKosloski <matthewkosloski@me>
2015-10-27 00:18:59 -0500

    show stats on mouseenter event

---
+++ src/js/directives/ahoraStats.js
@@ -0,0 +1,23 @@
+(function(){
+	angular.module("directives")
+		.directive("ahoraStats", ["animations", function(animations){
+		    return {
+		    	restrict : "A",
+		        link: function ($scope, element, attrs) {
+		        	var mainContent = $(".main__content"),
+						mainStats = $(".main__stats"),
+						duration = 250;
+					element.bind({
+						mouseenter: function(){
+							$(mainContent).fadeOut(duration);
+							$(mainStats).fadeIn(duration);
+						},
+						mouseleave: function(){
+							$(mainStats).fadeOut(duration);
+							$(mainContent).fadeIn(duration);
+						}
+					});
+		        }
+		    };
+		}]);
+})();
