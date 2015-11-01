79befb16aa6c85f6fbffbaa3ac482efb037b315d
MatthewKosloski <matthewkosloski@me>
2015-11-01 15:39:50 -0600

    hide stats on mobile

--- src/js/directives/ahoraStats.js
+++ src/js/directives/ahoraStats.js
@@ -7,16 +7,21 @@
 		        	var mainContent = $(".main__content"),
 						mainStats = $(".main__stats"),
 						duration = 250;
-					element.bind({
-						mouseenter: function(){
-							$(mainContent).fadeOut(duration);
-							$(mainStats).fadeIn(duration);
-						},
-						mouseleave: function(){
-							$(mainStats).fadeOut(duration);
-							$(mainContent).fadeIn(duration);
-						}
-					});
+
+					var browserWidth = $(window).width();
+
+					if(browserWidth >= 640) {
+						element.bind({
+							mouseenter: function(){
+								mainContent.fadeOut(duration);
+								mainStats.fadeIn(duration);
+							},
+							mouseleave: function(){
+								mainStats.fadeOut(duration);
+								mainContent.fadeIn(duration);
+							}
+						});
+					}
 		        }
 		    };
 		});
